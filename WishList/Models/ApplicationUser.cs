98adb1ddeaa47a4d3d52fb769b4bb271ac4f5d42
avasileva <avasileva@PC-AVASILEVA>
2022-12-19 10:55:33 +0200

    Add project files.

---
+++ WishList/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+namespace WishList.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public virtual ICollection<Item> Items { get; set; }
+    }
+}
